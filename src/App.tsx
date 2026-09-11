2e5d74c82fb3ba07709bf740a88c62a3e874ec0b
Muradul Islam <1053murad@gmail.com>
2026-09-11 20:12:53 +0600

    nav and banner section added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Banner from "./Components/Banner"
+import Nav from "./Components/Nav"
+
+
+function App() {
+
+  return (
+    <>
+    <Nav></Nav>
+    <Banner></Banner>
+
+    </>
+  )
+}
+
+export default App
